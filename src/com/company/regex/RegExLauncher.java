68268ed0648ce197ef9576e3f6d043e2b71e0591
Alexey Efimov <Rick_Headle@rambler.ru>
2021-06-20 00:18:45 +0500

    Переместил запуск проекта в класс RegExApplication с применением Spring Boot.

--- src/com/company/regex/RegExLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.company.regex;
-
-import com.company.regex.test.RegExTest;
-
-public class RegExLauncher {
-    public static void main(String[] args) {
-        RegExTest.checkFromConsole();
-    }
-}
--- src/main/java/com/example/regex/RegExApplication.java
+++ src/main/java/com/example/regex/RegExApplication.java
@@ -1,13 +1,19 @@
 package com.example.regex;
 
+import com.company.regex.test.RegExTest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegExApplication {
+public class RegExApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(RegExApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        RegExTest.checkFromConsole();
+    }
 }
