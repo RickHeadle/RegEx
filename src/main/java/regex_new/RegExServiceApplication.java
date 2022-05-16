7a233767dc928c731728912c878e753454924edc
Alexey Efimov <Rick_Headle@rambler.ru>
2022-05-16 22:41:04 +0500

    Исправление JpaRepositoryConfig, теперь main в RegExServiceApplication запускается.

--- src/main/java/regex_new/RegExServiceApplication.java
+++ src/main/java/regex_new/RegExServiceApplication.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,6 @@ import java.security.Principal;
 import java.util.Locale;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class RegExServiceApplication {
     public static void main(String[] args) {
         Locale.setDefault(Locale.forLanguageTag("RU"));
--- src/main/java/regex_new/config/JpaRepositoryConfig.java
+++ src/main/java/regex_new/config/JpaRepositoryConfig.java
@@ -1,13 +1,11 @@
 package regex_new.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableJpaRepositories
 @EnableTransactionManagement
-@EntityScan(basePackages = "regex_new")
-@EnableJpaRepositories(basePackages = "regex_new.repository")
 public class JpaRepositoryConfig {
 }
