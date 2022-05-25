a7ffa2d10f4945a32da9ee43b3dd105650b90134
Alexey Efimov <Rick_Headle@rambler.ru>
2022-05-26 04:31:18 +0500

    Первичная реализация SecurityConfig.java

---
+++ src/main/java/regex_new/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package regex_new.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auths -> auths
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
