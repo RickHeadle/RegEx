07caab6639c3336598d9746f917c851c8077a9cb
Alexey Efimov <Rick_Headle@rambler.ru>
2023-04-02 20:32:52 +0500

    (unfin!) Попытка восстановления работоспособности проекта.

--- src/main/java/regex_new/config/JpaRepositoryConfig.java
+++ src/main/java/regex_new/config/JpaRepositoryConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan(basePackages = "regex_new")
 @EntityScan(basePackages = "regex_new.entity")
--- src/main/java/regex_new/config/MvcConfig.java
+++ src/main/java/regex_new/config/MvcConfig.java
@@ -1,12 +1,12 @@
 package regex_new.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
+@EnableSpringDataWebSupport
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
